3ab5ab2195e13f866ac96dfbba34c310d324097b
Aastha Gupta <AasthaGupta@users.noreply.github.com>
2021-01-12 01:03:12 +0530

    fixes integer overflow while bitwise shifting (#10)
    Signed-off-by: Aastha Gupta <aastha.gupta4104@gmail.com>

--- Sources/RSocketCore/Frame Header/FrameHeaderEncoder.swift
+++ Sources/RSocketCore/Frame Header/FrameHeaderEncoder.swift
@@ -25,7 +25,7 @@ internal struct FrameHeaderEncoder: FrameHeaderEncoding {
         var buffer = allocator.buffer(capacity: FrameHeader.lengthInBytes)
         buffer.writeInteger(header.streamId)
         // shift type by amount of flag bits
-        let typeBits = UInt16(header.type.rawValue << 10)
+        let typeBits = UInt16(header.type.rawValue) << 10
         // only use trailing 10 bits for flags
         let flagBits = header.flags.rawValue & 0b0000001111111111
         buffer.writeInteger(typeBits | flagBits)

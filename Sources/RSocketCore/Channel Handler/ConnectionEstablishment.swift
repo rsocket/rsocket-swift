a47f0f7b08969d80f32df5de66cf94984d7f74c0
ciychodianda <chengappa_iychodianda@intuit.com>
2021-03-31 17:48:14 -0700

    incorporated code review comments
    Signed-off-by: ciychodianda <chengappa_iychodianda@intuit.com>

--- Sources/RSocketCore/Channel Handler/ConnectionEstablishment.swift
+++ Sources/RSocketCore/Channel Handler/ConnectionEstablishment.swift
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-import NIO
 import Foundation
+import NIO
 
 /// Information about a client which is about to connect or is connected.
 public struct SetupInfo {
     /// If the connection should honor `LEASE`
     public let honorsLease: Bool
-    
+
     /// version of the client protocol implementation
     public let version: Version
     /**
@@ -72,11 +72,11 @@ public struct SetupInfo {
 
 internal struct SetupValidator {
     internal var maximumClientVersion = Version.v1_0
-    
+
     internal func validate(frame: Frame) throws -> SetupInfo {
         try validateSetup(try getSetupBody(frame))
     }
-    
+
     private func getSetupBody(_ frame: Frame) throws -> SetupFrameBody {
         guard frame.header.streamId == .connection else {
             throw Error.invalidSetup(message: "connection needs to be setup on stream 0")
@@ -89,7 +89,7 @@ internal struct SetupValidator {
         }
         return setup
     }
-    
+
     private func validateSetup(_ setup: SetupFrameBody) throws -> SetupInfo {
         guard setup.version <= maximumClientVersion else {
             throw Error.unsupportedSetup(message: "only version \(maximumClientVersion) and lower are supported")
@@ -167,7 +167,6 @@ fileprivate final class MessageBufferHandler: ChannelInboundHandler, RemovableCh
 /// `ConnectionEstablishmentHandler` will remove itself after it the returned promise of `initializeConnection` is fulfilled.
 internal final class ConnectionEstablishmentHandler: ChannelInboundHandler, RemovableChannelHandler {
     typealias InboundIn = Frame
-    typealias InboundOut = Frame
     typealias OutboundOut = Frame
     
     private enum State {
@@ -184,7 +183,6 @@ internal final class ConnectionEstablishmentHandler: ChannelInboundHandler, Remo
     private var state: State = .idle
     
     private let setupValidator = SetupValidator()
-    
     
     /// Configure `ConnectionEstablishmentHandler`.  If `shouldAcceptClient` is nil, valid clients are always accepted.
     /// - Parameters:
@@ -228,7 +226,7 @@ internal final class ConnectionEstablishmentHandler: ChannelInboundHandler, Remo
                         // something failed in initializeConnection
                         self.writeErrorAndCloseConnection(context: context, error: error)
                     case .success:
-                        context.fireChannelRead(self.wrapInboundOut(frame))
+                        context.fireChannelActive()
                         context.pipeline.removeHandler(context: context).whenComplete { _ in
                             /// When we remove `messageBuffer` we'll be delivering any buffered
                             context.pipeline.removeHandler(messageBuffer, promise: nil)

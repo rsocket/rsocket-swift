3fe9956b0dba3101fa7b709d08d15f975e4802ca
David Nadoba <dnadoba@gmail.com>
2021-07-09 12:11:04 +0200

    split composite metadata into multiple files
    Signed-off-by: David Nadoba <dnadoba@gmail.com>

---
+++ Sources/RSocketCore/Extensions/Coder/Decoder/CompositeMetadataDecoder.swift
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+import NIO
+
+public protocol CompositeMetadataDecoder {
+    associatedtype Metadata
+    func decode(from compositeMetadata: [CompositeMetadata]) throws -> Metadata
+}
+
+
+public struct CompositeMetadataDecoderTuple2<A, B>: CompositeMetadataDecoder where
+A: MetadataDecoder,
+B: MetadataDecoder
+{
+    public typealias Metadata = (A.Metadata, B.Metadata)
+    @usableFromInline
+    internal let decoder: (A, B)
+    
+    @inlinable
+    public func decode(from compositeMetadata: [CompositeMetadata]) throws -> Metadata {
+        (
+            try compositeMetadata.decodeFirst(using: decoder.0),
+            try compositeMetadata.decodeFirst(using: decoder.1)
+        )
+    }
+}
+
+public struct CompositeMetadataDecoderTuple3<A, B, C>: CompositeMetadataDecoder where
+A: MetadataDecoder,
+B: MetadataDecoder,
+C: MetadataDecoder
+{
+    public typealias Metadata = (A.Metadata, B.Metadata, C.Metadata)
+    
+    @usableFromInline
+    internal let decoder: (A, B, C)
+    
+    @inlinable
+    public func decode(from compositeMetadata: [CompositeMetadata]) throws -> Metadata {
+        (
+            try compositeMetadata.decodeFirst(using: decoder.0),
+            try compositeMetadata.decodeFirst(using: decoder.1),
+            try compositeMetadata.decodeFirst(using: decoder.2)
+        )
+    }
+}
+
+@resultBuilder
+public enum CompositeMetadataDecoderBuilder {
+    public static func buildBlock<Decoder>(
+        _ decoder: Decoder
+    ) -> Decoder where Decoder: CompositeMetadataDecoder {
+        decoder
+    }
+    public static func buildBlock<A, B>(
+        _ a: A,
+        _ b: B
+    ) -> CompositeMetadataDecoderTuple2<A, B> {
+        .init(decoder: (a, b))
+    }
+    public static func buildBlock<A, B, C>(
+        _ a: A,
+        _ b: B,
+        _ c: C
+    ) -> CompositeMetadataDecoderTuple3<A, B, C> {
+        .init(decoder: (a, b, c))
+    }
+    public static func buildLimitedAvailability<T>(_ component: T) -> T {
+        component
+    }
+    public static func buildEither<T>(first component: T) -> T {
+        component
+    }
+    public static func buildEither<T>(second component: T) -> T {
+        component
+    }
+}
---
+++ Sources/RSocketCore/Extensions/Coder/Encoder/CompositeMetadataEncoder.swift
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2015-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+import NIO
+
+public protocol CompositeMetadataEncoder {
+    associatedtype Metadata
+    func encodeMetadata(_ metadata: Metadata) throws -> [CompositeMetadata]
+}
+
+public struct CompositeMetadataEncoderTuple2<A, B>: CompositeMetadataEncoder where
+A: MetadataEncoder,
+B: MetadataEncoder
+{
+    public typealias Metadata = (A.Metadata, B.Metadata)
+    
+    @usableFromInline
+    internal let encoder: (A, B)
+    
+    @inlinable
+    public func encodeMetadata(_ metadata: Metadata) throws -> [CompositeMetadata] {
+        [
+            try CompositeMetadata.encoded(metadata.0, using: encoder.0),
+            try CompositeMetadata.encoded(metadata.1, using: encoder.1),
+        ]
+    }
+}
+
+public struct CompositeMetadataEncoderTuple3<A, B, C>: CompositeMetadataEncoder where
+A: MetadataEncoder,
+B: MetadataEncoder,
+C: MetadataEncoder
+{
+    public typealias Metadata = (A.Metadata, B.Metadata, C.Metadata)
+    
+    
+    @usableFromInline
+    internal let encoder: (A, B, C)
+    
+    @inlinable
+    public func encodeMetadata(_ metadata: Metadata) throws -> [CompositeMetadata] {
+        [
+            try CompositeMetadata.encoded(metadata.0, using: encoder.0),
+            try CompositeMetadata.encoded(metadata.1, using: encoder.1),
+            try CompositeMetadata.encoded(metadata.2, using: encoder.2),
+        ]
+    }
+}
+
+
+@resultBuilder
+public enum CompositeMetadataEncoderBuilder {
+    public static func buildBlock<Encoder>(
+        _ encoder: Encoder
+    ) -> Encoder where Encoder: CompositeMetadataEncoder {
+        encoder
+    }
+    public static func buildBlock<A, B>(
+        _ a: A,
+        _ b: B
+    ) -> CompositeMetadataEncoderTuple2<A, B> {
+        .init(encoder: (a, b))
+    }
+    public static func buildBlock<A, B, C>(
+        _ a: A,
+        _ b: B,
+        _ c: C
+    ) -> CompositeMetadataEncoderTuple3<A, B, C> {
+        .init(encoder: (a, b, c))
+    }
+    public static func buildLimitedAvailability<T>(_ component: T) -> T {
+        component
+    }
+    public static func buildEither<T>(first component: T) -> T {
+        component
+    }
+    public static func buildEither<T>(second component: T) -> T {
+        component
+    }
+}

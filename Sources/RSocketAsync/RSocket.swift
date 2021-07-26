affca722787e098c7a56dfeeb3f313aa08951ae3
David Nadoba <dnadoba@gmail.com>
2021-07-26 21:29:47 +0200

    split into multiple files
    Signed-off-by: David Nadoba <dnadoba@gmail.com>

---
+++ Sources/RSocketAsync/RSocket.swift
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if compiler(>=5.5)
+import Foundation
+import RSocketCore
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public protocol RSocket {
+    func metadataPush(metadata: Data)
+    func fireAndForget(payload: Payload)
+    func requestResponse(payload: Payload) async throws -> Payload
+    func requestStream(payload: Payload) -> AsyncThrowingStream<Payload, Swift.Error>
+    func requestChannel<PayloadSequence>(
+        initialPayload: Payload, 
+        payloadStream: PayloadSequence
+    ) -> AsyncThrowingStream<Payload, Swift.Error> 
+    where PayloadSequence: AsyncSequence, PayloadSequence.Element == Payload
+}
+
+#endif
--- Sources/RSocketAsync/Requester.swift
+++ Sources/RSocketAsync/Requester.swift
@@ -1,22 +1,24 @@
+/*
+ * Copyright 2015-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #if compiler(>=5.5)
 import Foundation
 import NIO
 import RSocketCore
-import _Concurrency
 import _NIOConcurrency
-
-@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
-public protocol RSocket {
-    func metadataPush(metadata: Data)
-    func fireAndForget(payload: Payload)
-    func requestResponse(payload: Payload) async throws -> Payload
-    func requestStream(payload: Payload) -> AsyncThrowingStream<Payload, Swift.Error>
-    func requestChannel<PayloadSequence>(
-        initialPayload: Payload, 
-        payloadStream: PayloadSequence
-    ) -> AsyncThrowingStream<Payload, Swift.Error> 
-    where PayloadSequence: AsyncSequence, PayloadSequence.Element == Payload
-}
 
 @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public struct RequesterAdapter: RSocket {
@@ -156,21 +158,4 @@ internal final class AsyncStreamAdapter: UnidirectionalStream {
     }
 }
 
-@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
-public struct AsyncClient {
-    private let coreClient: RSocketCore.CoreClient
-
-    public var requester: RSocket { RequesterAdapter(requester: coreClient.requester) }
-
-    public init(_ coreClient: RSocketCore.CoreClient) {
-        self.coreClient = coreClient
-    }
-}
-
-@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
-extension RSocketCore.ClientBootstrap where Client == CoreClient, Responder == RSocketCore.RSocket  {
-    public func connect(to endpoint: Transport.Endpoint, payload: Payload) async throws -> AsyncClient {
-        AsyncClient(try await connect(to: endpoint, payload: payload, responder: nil).get())
-    }
-}
 #endif

46aabf433be40b28505dc0537b8c0bce29d43915
David Nadoba <dnadoba@gmail.com>
2021-08-25 11:37:54 +0200

    add convenient way to convert ByteBuffer to/from Data in Decoder/Encoder pipeline
    Signed-off-by: David Nadoba <dnadoba@gmail.com>

---
+++ Sources/RSocketCore/Extensions/Coder/Decoder/Decoder+AsData.swift
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+import NIOFoundationCompat
+import NIOCore
+
+extension Decoders {
+    public struct AsData<Decoder: DecoderProtocol>: DecoderProtocol where Decoder.Data == ByteBuffer {
+        @usableFromInline
+        internal var decoder: Decoder
+
+        @usableFromInline
+        internal var byteTransferStrategy: ByteBuffer.ByteTransferStrategy
+
+        @inlinable
+        internal init(decoder: Decoder, byteTransferStrategy: ByteBuffer.ByteTransferStrategy) {
+            self.decoder = decoder
+            self.byteTransferStrategy = byteTransferStrategy
+        }
+
+        @inlinable
+        public mutating func decode(
+            _ payload: Payload,
+            encoding: ConnectionEncoding
+        ) throws -> (Decoder.Metadata, Foundation.Data) {
+            let (metadata, data) = try decoder.decode(payload, encoding: encoding)
+            return (metadata, Data(buffer: data, byteTransferStrategy: byteTransferStrategy))
+        }
+    }
+}
+
+extension DecoderProtocol where Data == ByteBuffer {
+    @inlinable
+    public func asData(
+        byteTransferStrategy: ByteBuffer.ByteTransferStrategy
+    ) -> Decoders.AsData<Self> {
+        .init(decoder: self, byteTransferStrategy: byteTransferStrategy)
+    }
+}

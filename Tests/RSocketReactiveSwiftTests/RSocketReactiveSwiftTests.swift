5003f15f7e72b58e22d38dc14ade57378d18e007
ayush-yadav001 <105836765+ayush-yadav001@users.noreply.github.com>
2022-08-19 18:22:14 +0530

    Adds observer api for closing connection event (#68)
    Co-authored-by: Niclas Kristek <dev@nkristek.com>

--- Tests/RSocketReactiveSwiftTests/RSocketReactiveSwiftTests.swift
+++ Tests/RSocketReactiveSwiftTests/RSocketReactiveSwiftTests.swift
@@ -404,4 +404,46 @@ final class RSocketReactiveSwiftTests: XCTestCase {
         // checking if connection is inactive
         XCTAssertTrue(client.isDisposed)
     }
+    func testConnectionDisposeListener() {
+        // Creating expectation
+        let didReceiveConnectionclosedEvent = expectation(description: "did receive Connection closed  event ")
+        let serverResponder: RSocketReactiveSwift.ResponderRSocket? = TestRSocket()
+        let clientResponder: RSocketReactiveSwift.ResponderRSocket? = TestRSocket()
+        let (serverMultiplexer, clientMultiplexer) = TestDemultiplexer.pipe(
+            serverResponder: serverResponder.map { ResponderAdapter(responder: $0, encoding: .default) },
+            clientResponder: clientResponder.map { ResponderAdapter(responder: $0, encoding: .default) }
+        )
+        // Channel creation
+        let serverChannel = EmbeddedChannel()
+        // Making channel Active
+        XCTAssertNoThrow(try serverChannel.connect(to: SocketAddress.init(ipAddress: "127.0.0.1", port: 0)).wait())
+        let clientChannel = EmbeddedChannel()
+        XCTAssertNoThrow(try clientChannel.connect(to: SocketAddress.init(ipAddress: "127.0.0.1", port: 0)).wait())
+        // Creating Reactive swift client
+        let server =  ReactiveSwiftClient(CoreClient(requester: serverMultiplexer.requester, channel: serverChannel))
+        let client =  ReactiveSwiftClient(CoreClient(requester: clientMultiplexer.requester, channel: clientChannel))
+        defer {
+            // closing channel connection
+            XCTAssertNoThrow(try serverChannel.finish())
+        }
+        XCTAssertNotNil(server)
+        XCTAssertNotNil(client)
+        // client connection closed event signal producer
+        client.shutdownProducer.startWithSignal({ signal, interruptHandle in
+            signal.flatMapError({ error -> Signal<Void, Error> in
+                XCTFail("\(error)")
+                return .empty
+            }).materialize().collect().observeValues { values in
+                didReceiveConnectionclosedEvent.fulfill()
+            }
+        })
+        // checking if connection is active
+        XCTAssertFalse(client.isDisposed)
+        // closing connection using dispose method
+        XCTAssertNoThrow(try clientChannel.finish())
+        // checking if connection is inactive
+        XCTAssertTrue(client.isDisposed)
+        self.wait(for: [didReceiveConnectionclosedEvent], timeout: 0.1)
+    }
 }
+
